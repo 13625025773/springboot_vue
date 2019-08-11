969aab811d61dec7f26650cfd609dc6a5d95d463
13625025773 <975950982@qq.com>
2019-08-11 09:22:27 +0800

    undertow 配置
    Unertow 是一个红帽公司开源的 Java 服务器,具有非常好的性能

---
+++ undertow/src/main/java/com/example/undertow/UndertowApplication.java
@@ -0,0 +1,21 @@
+package com.example.undertow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class UndertowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UndertowApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, undertow";
+    }
+
+}
